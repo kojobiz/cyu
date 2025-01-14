605802eb8133ad319deb6b55d16b2a8302d6dd72
lumijoe <spacedesign63@gmail.com>
2025-01-14 14:03:16 +0900

    test

---
+++ footercont.php
@@ -0,0 +1,149 @@
+<?php
+$locale = get_locale();
+if ('en_US' == $locale) {
+?>
+	<!--英語のフッター表示-->
+	<div class="chgr-en-ft">
+		<div class="chgr-en-ft-left">
+			<ul>
+				<li><a href="//chugai.co.jp/en/"><span class="chgr-en-ft-fnt10">Home</span></a></li>
+			</ul>
+			<ul class="chgr-en-ft-line">
+				<li><span class="chgr-en-ft-fnt10">Products</span>
+					<ul>
+						<li><a href="//chugai.co.jp/en/pro_01_parts_01/">Heat Treatment Furnace<br>for Parts and Materials</a>
+						<li><a href="//chugai.co.jp/en/pro_05_steel_01/">Plant for Steels and Non-steel Metals</a>
+						<li><a href="//chugai.co.jp/en/pro_04_3_gas_01/">Burner and Combustion<br>Control Equipment</a>
+						<li><a href="//chugai.co.jp/en/env_11_biomass_01/">Air Pollution Control System</a>
+						<li><a href="//chugai.co.jp/en/env_11_biomass_01/">Kiln and Environmental Equipment</a>
+						<li><a href="//chugai.co.jp/en/coater_search/">Substrate/Film Coating and<br>Drying Equipment</a>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="chgr-en-ft-mid">
+			<ul>
+				<li><a href="//chugai.co.jp/en/news/"><span class="chgr-en-ft-fnt10">News</span></a></li>
+			</ul>
+			<ul>
+				<li><a href="//chugai.co.jp/en/ir_greeting/"><span class="chgr-en-ft-fnt10">Corporate profile, IR</span></a></li>
+			</ul>
+			<ul>
+				<li><a href="//chugai.co.jp/contact_form/"><span class="chgr-en-ft-fnt10">Inquiry</span></a></li>
+			</ul>
+		</div>
+		<div class="chgr-en-ft-right">
+			<ul class="chgr-en-ft-line">
+				<li><span class="chgr-en-ft-fnt10">Related companies/organizations</span>
+					<ul>
+						<li><a href="https://chugai-plant.co.jp/" target="_blank">CHUGAI PLANT CO., LTD.</a></li>
+						<li><a href="http://chugairo.com.tw/" target="_blank">TAIWAN CHUGAI RO CO., LTD.</a></li>
+						<li><a href="https://ja.nc-net.or.jp/company/103708/" target="_blank">CHUGAI RO (THAILAND) CO., LTD.</a></li>
+						<li><a href="https://chugairoindonesia.com/" target="_blank">PT. CHUGAI RO INDONESIA</a></li>
+						<li><a href="https://chugairo-mexico.com/" target="_blank">CHUGAI RO DE MEXICO, S.A. DE C.V.</a></li>
+					</ul>
+				</li>
+			</ul>
+			<div class="chgr-en-ft-sns">
+				<a href="https://twitter.com/CHUGAIRO_pr" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/x_logo_pc.png" /></a>
+				<a href="https://www.youtube.com/@chugairo9408" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/yt_logo_pc.png" /></a>
+			</div>
+		</div>
+	</div>
+<?php } else { ?>
+	<!--日本語のフッター表示-->
+	<div class="chgr-jp-ft">
+		<div class="chgr-jp-ft-left">
+			<ul class="chgr-jp-ft-line">
+				<li><span class="chgr-jp-ft-fnt10">製品情報</span>
+					<ul>
+						<li><a href="//chugai.co.jp/heat_treat_search/">部品・素材熱処理炉</a></li>
+						<li><a href="//chugai.co.jp/plant/">鉄鋼・非鉄金属プラント</a></li>
+						<li><a href="//chugai.co.jp/burner/">バーナ・燃焼制御機器</a></li>
+						<li><a href="//chugai.co.jp/coater_search/">基板・フィルム塗工乾燥装置</a></li>
+						<li><a href="//chugai.co.jp/environment/">大気浄化設備</a></li>
+						<li><a href="//chugai.co.jp/kiln-equipment/">キルン・汚泥焼却設備</a></li>
+					</ul>
+				</li>
+			</ul>
+			<ul class="chgr-jp-ft-line">
+				<li><span class="chgr-jp-ft-fnt10">ニュース</span>
+					<ul>
+						<li><a href="//chugai.co.jp/news/">ニュース一覧</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="chgr-jp-ft-mid1">
+			<ul>
+				<li><span class="chgr-jp-ft-fnt10">企業情報・IR</span>
+					<ul class="chgr-jp-ft-line">
+						<li><span class="chgr-jp-ft-fnt20">企業情報</span>
+							<ul>
+								<li><a href="//chugai.co.jp/ir_greeting/">ごあいさつ</a></li>
+								<li><a href="//chugai.co.jp/ir_kankyo/">経営理念と基本方針</a></li>
+								<li><a href="//chugai.co.jp/crp_01gaiyo/">会社概要／国内拠点</a></li>
+								<li><a href="//chugai.co.jp/ir_network/">国内／海外子会社・<br>グローバルパートナー</a></li>
+								<li><a href="//chugai.co.jp/crp_03jigyo/">事業内容</a></li>
+								<li><a href="//chugai.co.jp/core_tech/">中外炉工業のコア技術</a></li>
+								<li><a href="//chugai.co.jp/decarbonize/">中外炉工業の脱炭素<br>技術への取り組み</a></li>
+								<li><a href="//chugai.co.jp/ir_history-21/">中外炉ヒストリー</a></li>
+								<li><a href="//chugai.co.jp/crp_09installations/">海外実績</a></li>
+								<li><a href="//chugai.co.jp/ir_cg/">コーポレートガバナンス</a></li>
+								<li><a href="//chugai.co.jp/outline-of-the-company/">行動規範</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="chgr-jp-ft-mid2">
+			<ul>
+				<li><span>&nbsp;</span>
+					<ul class="chgr-jp-ft-line">
+						<li><span class="chgr-jp-ft-fnt20">IR 情報</span>
+							<ul>
+								<li><a href="//chugai.co.jp/ir_financial_briefing/">決算説明資料</a></li>
+								<li><a href="//chugai.co.jp/ir_mid_plan/">中期経営計画</a></li>
+								<li><a href="//chugai.co.jp/ir_message/">業績概況</a></li>
+								<li><a href="//chugai.co.jp/ir_soukai/">株主総会</a></li>
+								<li><a href="//chugai.co.jp/ir_report/">株主様向け報告書等</a></li>
+								<li><a href="//chugai.co.jp/ir_s-report/">有価証券報告書</a></li>
+								<li><a href="//chugai.co.jp/ir_stock/">株式および配当金情報</a></li>
+								<li><a href="//chugai.co.jp/ir_koukoku/">電子公告</a></li>
+								<li><a href="//chugai.co.jp/ir_faq/">よくあるご質問（FAQ）</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="chgr-jp-ft-right">
+			<ul class="chgr-jp-ft-line">
+				<li><span class="chgr-jp-ft-fnt10">関連企業・団体</span>
+					<ul>
+						<li><a href="https://chugai-plant.co.jp/" target="_blank">中外プラント株式会社</a></li>
+						<li><a href="http://chugairo.com.tw/" target="_blank">TAIWAN CHUGAI RO CO., LTD.</a></li>
+						<li><a href="https://ja.nc-net.or.jp/company/103708/" target="_blank">CHUGAI RO (THAILAND) CO., LTD.</a></li>
+						<li><a href="https://chugairoindonesia.com/" target="_blank">PT. CHUGAI RO INDONESIA</a></li>
+						<li><a href="https://chugairo-mexico.com/" target="_blank">CHUGAI RO DE MEXICO, S.A. DE C.V.</a></li>
+						<li><a href="http://chugairo.net/ob/" target="_blank">中外炉ＯＢクラブホームページ</a></li>
+					</ul>
+				</li>
+			</ul>
+			<ul class="chgr-jp-ft-line">
+				<li><span class="chgr-jp-ft-fnt10">研究・開発・その他コンテンツ</span>
+					<ul>
+						<li><a href="//chugai.co.jp/ttcl_intro/">受託テストのご案内</a></li>
+						<li><a href="//chugai.co.jp/giroku_archive/">中外技録</a></li>
+						<li><a href="//chugai.co.jp/naani/">サーモテックってなあに？</a></li>
+					</ul>
+				</li>
+			</ul>
+			<div class="chgr-jp-ft-sns">
+				<a href="https://twitter.com/CHUGAIRO_pr" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/x_logo_pc.png" /></a>
+				<a href="https://www.youtube.com/@chugairo9408" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/yt_logo_pc.png" /></a>
+			</div>
+		</div>
+	</div>
+<?php } ?>
